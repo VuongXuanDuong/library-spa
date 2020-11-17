a8c5fd5c2fe5e32f28715c5b85831b20e68f4085
xuanduongvuong <kysunam@teneocto.io>
2020-11-17 17:03:28 +0700

    add function in BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -10,11 +11,12 @@ class BookController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return array
      */
     public function index()
     {
-        //
+        $books = Book::all()->toArray();
+        return array_reverse($books);
     }
 
     /**
@@ -30,56 +32,72 @@ class BookController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $book = new Book([
+            'name' => $request->input('name'),
+            'author' => $request->input('author')
+        ]);
+        $book->save();
+
+        return response()->json([
+            'message' => 'Store successful',
+            'status-code' => 200
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
+     * @param Book $book
+     * @return JsonResponse
      */
     public function show(Book $book)
     {
-        //
+        return response()->json($book);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
+     * @param Book $book
+     * @return JsonResponse
      */
     public function edit(Book $book)
     {
-        //
+        return response()->json($book);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Book $book
+     * @return JsonResponse
      */
     public function update(Request $request, Book $book)
     {
-        //
+        $book->update($request->all());
+        return response()->json([
+            'message' => 'The book successfully',
+            'status-code' => 200
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
+     * @param Book $book
+     * @return JsonResponse
      */
     public function destroy(Book $book)
     {
-        //
+        return response()->json([
+            'message'=> 'The book successfully deleted!',
+            'status-code' => 200
+        ]);
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/duong/Documents/train-laravel/vue-spa/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/duong/Documents/train-laravel/vue-spa/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /home/duong/Documents/train-laravel/library-spa/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/duong/Documents/train-laravel/library-spa/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
